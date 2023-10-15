2c4393120a2cd670694a91a99335d1d36aa94fcf
m7mdhusinn <m7md.husinn@gmail.com>
2023-10-15 21:02:24 +0300

    lab of function102 is done

--- find__primes.py
+++ find__primes.py
@@ -1,9 +1,11 @@
 def find_primes(num1:int ,num2:int) -> int:
 
     for n in range(num1,num2+1):
-        if n % 2 != 0 and n%3 != 0 and n%4 !=0 and n%5 != 0 and n%6 !=0 and n%7 !=0 and n%8 !=0 and n%9 !=0 :
-          
-            print (n) 
+        for n2 in range(2,n):
+            if n%n2==0:
+                break
+        else:
+            print(n)
 number1=int(input("please enter the first number: "))
 number2=int(input("please enter the second number: "))             
 find_primes(number1,number2)    
